1ec3bf313caf8115cf3f38f821e77a9ddb4c108b
georgiiviktorov <nowayisleft@gmail.com>
2025-04-19 13:21:16 +0400

    redis workable approx

--- src/main.py
+++ src/main.py
@@ -22,3 +22,5 @@ async def lifespan(a: FastAPI):
 app = FastAPI()  # run -> uvicorn src.main:app
 
 from src.api import api
+
+# load testing -> wrk -t4 -c40 -d10s -H "accept: application/json" "http://127.0.0.1:8000/records?skip=0&limit=10"
